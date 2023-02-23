34b3e2cf1d7452c5e01c474528cb22229ca89c3c
StroeAndrei <str0e_andrei@yahoo.com>
2023-02-23 15:23:28 +0200

    23.2.2023

--- AppWeather/src/main/java/controllers/WeatherController.java
+++ AppWeather/src/main/java/controllers/WeatherController.java
@@ -12,25 +12,26 @@ import proxy.WeatherProxy;
 
 @Controller
 public class WeatherController {
-	
+
 	private final WeatherProxy weatherProxy;
-	
+
 	private static final Logger logger = Logger.getLogger(WeatherController.class.getName());
-	
+
 	public WeatherController(WeatherProxy weatherProxy) {
 		this.weatherProxy = weatherProxy;
 	}
-	
+
 	@GetMapping("/weather")
-    public String weather(Model model) {
-        Weather weather = weatherProxy.getWeather(44.42, 26.10, true, Arrays.asList("temperature_2m", "relativehumidity_2m", "windspeed_10m"));
-        
-        logger.info("Received weather data:" + weather.toString());
-        
-        model.addAttribute("temperature", weather.getCurrent_weather().getTemperature());
-        model.addAttribute("windspeed", weather.getCurrent_weather().getWindspeed());
-        model.addAttribute("date", weather.getCurrent_weather().getTime());
-        return "weather";
-    }
-	
+	public String weather(Model model) {
+		Weather weather = weatherProxy.getWeather(44.42, 26.10, true,
+				Arrays.asList("temperature_2m", "relativehumidity_2m", "windspeed_10m"));
+
+		logger.info("Received weather data:" + weather.toString());
+
+		model.addAttribute("temperature", weather.getCurrent_weather().getTemperature());
+		model.addAttribute("windspeed", weather.getCurrent_weather().getWindspeed());
+		model.addAttribute("date", weather.getCurrent_weather().getTime());
+		return "weather";
+	}
+
 }
